f11c58b61fa90afcd18d1ae6851882b380e73813
danny crasto <danwald79@gmail.com>
2021-03-06 18:05:55 +0400

    added hacked code

--- saxtract/cli.py
+++ saxtract/cli.py
@@ -1,6 +1,9 @@
 """Console script for saxtract."""
+#!/usr/bin/env python3
+
 import sys
 import click
+from saxtrack.saxtract import SAXHandler
 
 
 @click.command()
@@ -9,7 +12,16 @@ def main(args=None):
     click.echo("Replace this message by putting your code into "
                "saxtract.cli.main")
     click.echo("See click documentation at https://click.palletsprojects.com/")
-    return 0
+     # create an XMLReader
+    parser = xml.sax.make_parser()
+    # turn off namepsaces
+    parser.setFeature(xml.sax.handler.feature_namespaces, 0)
+
+    # override the default ContextHandler
+    handler = SAXHandler()
+    parser.setContentHandler(handler)
+    parser.parse(sys.stdin)
+	return 0
 
 
 if __name__ == "__main__":
--- saxtract/saxtract.py
+++ saxtract/saxtract.py
@@ -1,1 +1,23 @@
 """Main module."""
+'''plagirised from https://www.tutorialspoint.com/python3/python_xml_processing.htm'''
+from xml import sax
+
+class SAXHandler(sax.ContentHandler):
+    def __init__(self):
+        self.CurrentData = ""
+        self.xtag = ""
+
+    # Call when an element starts
+    def startElement(self, tag, attributes):
+        self.CurrentData = tag
+
+    # Call when an elements ends
+    def endElement(self, tag):
+        if self.CurrentData in ("refno",'listing_pk',):
+            print(self.xtag)
+            self.CurrentData = ""
+
+    # Call when a character is read
+    def characters(self, content):
+        if self.CurrentData in ("refno",'listing_pk',):
+            self.xtag = content
