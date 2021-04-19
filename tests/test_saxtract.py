a5da4e837f5e0cbed6293f4dacffa85e45369f5e
danny crasto <danwald79@gmail.com>
2021-04-19 22:39:58 +0400

    fixes tests moving it to a file and reading from there instead of stdin

--- tests/test_saxtract.py
+++ tests/test_saxtract.py
@@ -1,51 +1,20 @@
 #!/usr/bin/env python
 
 """Tests for `saxtract` package."""
+import io
+from pathlib import Path
 
 import pytest
-
 from click.testing import CliRunner
 
 from saxtract import saxtract
 from saxtract import cli
 
-XML_DATA= """
-<foo>
-    <bar>
-        <name>shamrocks</name>
-        <address>lexington;ky;usa;</address>
-    </bar>
-    <bar>
-        <name>irish-village</name>
-        <address>gharoud;dubai;uae;</address>
-    </bar>
-    <bar>
-        <name>garden-on-8</name>
-        <address>dubai-internet-city;ldubai;uae;</address>
-    </bar>
-</foo>
-"""
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
-
-
-def test_command_line_interface():
+def test_command_line_interface(monkeypatch):
     """Test the CLI."""
     runner = CliRunner()
-    result = runner.invoke(cli.main, input=XML_DATA)
+    in_path = Path(__file__).parent / 'data' / 'bars.xml'
+    result = runner.invoke(cli.main, args=['--instream', f'{in_path.absolute()}'])
     assert not result.exception
     assert result.exit_code == 0
-    #assert '--help  Show this message and exit.' in help_result.output
