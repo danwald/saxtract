31bb0d56dc0e8b1ea1b0ba53d52d4ae68d31d682
danny crasto <danwald79@gmail.com>
2021-04-22 19:00:34 +0400

    added show-tags test

--- tests/test_saxtract.py
+++ tests/test_saxtract.py
@@ -13,12 +13,22 @@ from saxtract import cli
 
 @pytest.fixture
 def test_file_path(autouse=True):
-    return Path(__file__).parent / 'data' / 'bars.xml'.absolute()
+    return (Path(__file__).parent / 'data' / 'bars.xml').absolute()
 
 
 def test_cli_defaults():
     """Test the CLI."""
     runner = CliRunner()
-    result = runner.invoke(cli.main, args=['--instream', f'{test_file_path}'])
+    fp = (Path(__file__).parent / 'data' / 'bars.xml').absolute()
+    result = runner.invoke(cli.main, args=['--instream', f'{fp}'])
+    assert not result.exception
+    assert result.exit_code == 0
+
+
+def test_cli_show_tags():
+    """Test the CLI."""
+    runner = CliRunner()
+    fp = (Path(__file__).parent / 'data' / 'bars.xml').absolute()
+    result = runner.invoke(cli.main, args=['--instream', f'{fp}', '--show-tags'])
     assert not result.exception
     assert result.exit_code == 0
