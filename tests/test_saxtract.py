ff5b28e49396b6783960c39186e15dd0312802b4
danny crasto <danwald79@gmail.com>
2021-04-22 18:37:09 +0400

    removing unused param from test

--- tests/test_saxtract.py
+++ tests/test_saxtract.py
@@ -11,10 +11,14 @@ from saxtract import saxtract
 from saxtract import cli
 
 
-def test_command_line_interface(monkeypatch):
+@pytest.fixture
+def test_file_path(autouse=True):
+    return Path(__file__).parent / 'data' / 'bars.xml'.absolute()
+
+
+def test_command_line_runs():
     """Test the CLI."""
     runner = CliRunner()
-    in_path = Path(__file__).parent / 'data' / 'bars.xml'
-    result = runner.invoke(cli.main, args=['--instream', f'{in_path.absolute()}'])
+    result = runner.invoke(cli.main, args=['--instream', f'{test_file_path}'])
     assert not result.exception
     assert result.exit_code == 0
