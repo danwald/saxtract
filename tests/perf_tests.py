3e708359188e907e22303378c72c22a35b5f6c16
danny crasto <danwald79@gmail.com>
2021-06-08 06:36:43 +0400

    add plumbing for tests

--- tests/perf_tests.py
+++ tests/perf_tests.py
@@ -7,18 +7,19 @@ import click
 
 from saxtract.saxtract import Saxtract
 
-
 @click.command()
 @click.option('--filename', prompt='Enter your test xml file')
 @click.option('--tag', prompt='Enter the tag you wish to extract')
 def main(filename, tag):
     runs = 1
     sax_time = Timer('sax(filename, tag)',
-                     setup=(f'filename="{filename}"; tag="{tag}";'),
+                     setup=(f'from __main__ import sax, dom;filename="{filename}"; tag="{tag}";'),
                      globals=globals(),
                      ).timeit(number=runs)
-    dom_time = Timer('sax(filename, tag)',
-                     f'from __main__ import sax; filename={filename}; tag={tag}').timeit(number=runs)
+    dom_time = Timer('dom(filename, tag)',
+                     setup=(f'from __main__ import sax, dom;filename="{filename}"; tag="{tag}";'),
+                     globals=globals(),
+                     ).timeit(number=runs)
     print(f'Sax run took ~{sax_time}ms Dom run took ~{dom_time}ms')
 
 
